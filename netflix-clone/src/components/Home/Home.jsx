7769bf79cc1b7b2514a3cc1c8146244a7aef7de3
Gaurav <grvjain56@gmail.com>
2022-11-07 09:27:53 +0530

    Trailers Added on Hover

--- netflix-clone/src/components/Home/Home.jsx
+++ netflix-clone/src/components/Home/Home.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Featured from '../Featured/Featured';
+// import Register from '../Register/Register' ;
 import "./Home.css" ;
 import MoviesLists from '../MoviesLists/MoviesLists';
 import List from '../List/List';
     
 const Home = () => {
     return ( 
+        
         <div className="home">
+            {/* <Register></Register> */}
             <Navbar></Navbar>
             <Featured></Featured>
             <List type="Horror" id="27"></List>
             <List type="Crime" id="80"></List>
-            <List type="Documentary" id="99"></List>
+            {/* <List type="Documentary" id="99"></List> */}
             <List type="Action" id="28"></List>
             <List type="Family" id="10751"></List>
             <List type="Fantasy" id="14"></List>
--- netflix-clone/src/components/MoviesLists/MoviesLists.jsx
+++ netflix-clone/src/components/MoviesLists/MoviesLists.jsx
@@ -1,3 +1,153 @@
+// import React, { useState, useEffect } from 'react';
+// import "./MoviesLists.css" ;
+// import { API_KEY , API_URL , IMAGE_URL } from "../../API/secrets" ;
+// import {
+//     PlayArrow,
+//     Add,
+//     ThumbUpAltOutlined,
+//     ThumbDownOutlined,
+//   } from "@mui/icons-material" ;
+
+
+// const MoviesLists = (props) => {
+//     const [response , setdata] = useState({}) ;
+//     let poster = `https://image.tmdb.org/t/p/w500${props.movies.poster_path}` ;
+
+//     const [isHovered, setIsHovered] = useState(false);
+//     const trailer = "https://www.youtube.com/watch?v=mDgEqoQUBgk&t=9s";
+
+//     return ( 
+//         <div className="movielist"
+//         // style={{ left: isHovered && props.index * 225 - 50 + props.index * 2.5 }}
+//         onMouseEnter={() => setIsHovered(true)}
+//         onMouseLeave={() => setIsHovered(false)}
+//         >
+//             <img src={poster} alt="" className="movieimg" />
+
+//         {isHovered && (
+//         <>
+//         <div className="movieHover"
+//         style={{ left: isHovered && props.index * 225 - 35 + props.index * 2.5 }}
+//         >
+            
+//             {/* <video className="video-class" src={trailer} autoPlay={true} loop /> */}
+//             <iframe className="video-class" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1&loop=1&modestbranding=1&;controls=0"></iframe>
+//             {/* <img src={poster} alt="" className="movieimg" /> */}
+//             <div className="itemInfo">
+//                 <div className="icons">
+//                 <PlayArrow className="playy" />
+//                 <Add />
+//                 <ThumbUpAltOutlined />
+//                 <ThumbDownOutlined  />
+//                 </div>
+//                 <div className="itemInfoTop">
+//                 <span>1 hour 14 mins</span>
+//                 <span className="limit">+16</span>
+//                 <span>1999</span>
+//                 </div>
+//                 <div className="desc">
+//                     {props.movies.overview}
+//                 </div>
+//                 <div className="genre">Action</div>
+//             </div>
+//         </div>
+//         </>
+//         )}
+//         </div>
+//      );
+// }
+ 
+// export default MoviesLists;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import "./MoviesLists.css" ;
+// import { API_KEY , API_URL , IMAGE_URL } from "../../API/secrets" ;
+// import {
+//     PlayArrow,
+//     Add,
+//     ThumbUpAltOutlined,
+//     ThumbDownOutlined,
+//   } from "@mui/icons-material" ;
+//   import YouTube from "react-youtube";
+
+
+// const MoviesLists = (props) => {
+//     const [response , setdata] = useState({}) ;
+//     let poster = `https://image.tmdb.org/t/p/w500${props.movies.poster_path}` ;
+
+//     const [isHovered, setIsHovered] = useState(false);
+//     // const trailer = "https://www.youtube.com/watch?v=mDgEqoQUBgk&t=9s";
+
+//     const[vObject , setObject] = useState({}) ;
+
+//     useEffect(() => {
+//         // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=b7c5d92115fce280b185d643ac4d4dfb&language=en-US
+
+//         const fetchData = async() => {
+//             const data = await fetch(`${API_URL}/movie/${props.movies.id}/videos?api_key=${API_KEY}&language=en-US`) ;
+//             const response = await data.json() ;
+//             // console.log(response) ;
+//             let videoObject = await response.results.filter((videoObj) => {
+//                 if (videoObj.type == "Trailer" && videoObj.site == "YouTube") {
+//                   return true;
+//                 }
+//                 return false;
+//             });
+//             setObject(videoObject[0]) ;
+//         }
+//         fetchData() ;
+//     })
+
+//     // const trailer = `https://www.youtube.com/embed/${vObject.key}?autoplay=1&mute=1&loop=1&modestbranding=1&;controls=0` ;
+//     // const trailer = 'https://www.youtube.com/embed/${vObject.key}?autoplay=1' ;
+
+
+//     return ( 
+//         <div className="movielist"
+//         // style={{ left: isHovered && props.index * 225 - 50 + props.index * 2.5 }}
+//         onMouseEnter={() => setIsHovered(true)}
+//         onMouseLeave={() => setIsHovered(false)}
+//         >
+//             <img src={poster} alt="" className="movieimg" />
+
+//         {isHovered && (
+//         <>
+//         <div className="movieHover"
+//         style={{ left: isHovered && props.index * 225 - 35 + props.index * 2.5 }}
+//         >
+            
+//             <iframe className="video-class" src={`https://www.youtube.com/embed/${vObject.key}?autoplay=1&mute=1&loop=1&controls=0&vq=low&modestbranding=1`}></iframe>
+//             {/* <YouTube className="video-class" videoId={vObject.key}></YouTube> */}
+//             {/* <iframe className="video-class" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1&loop=1&modestbranding=1&;controls=0"></iframe> */}
+//             <div className="itemInfo">
+//                 <div className="icons">
+//                 <PlayArrow className="playy" />
+//                 <Add />
+//                 <ThumbUpAltOutlined />
+//                 <ThumbDownOutlined  />
+//                 </div>
+//                 <div className="itemInfoTop">
+//                 <span>1 hour 14 mins</span>
+//                 <span className="limit">+16</span>
+//                 <span>1999</span>
+//                 </div>
+//                 <div className="desc">
+//                     {props.movies.overview}
+//                 </div>
+//                 <div className="genre">Action</div>
+//             </div>
+//         </div>
+//         </>
+//         )}
+//         </div>
+//      );
+// }
+ 
+// export default MoviesLists;
+
+
 import React, { useState, useEffect } from 'react';
 import "./MoviesLists.css" ;
 import { API_KEY , API_URL , IMAGE_URL } from "../../API/secrets" ;
@@ -7,6 +157,7 @@ import {
     ThumbUpAltOutlined,
     ThumbDownOutlined,
   } from "@mui/icons-material" ;
+  import YouTube from "react-youtube";
 
 
 const MoviesLists = (props) => {
@@ -14,11 +165,40 @@ const MoviesLists = (props) => {
     let poster = `https://image.tmdb.org/t/p/w500${props.movies.poster_path}` ;
 
     const [isHovered, setIsHovered] = useState(false);
-    const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+    // const trailer = "https://www.youtube.com/watch?v=mDgEqoQUBgk&t=9s";
+
+    const[vObject , setObject] = useState({}) ;
+
+    useEffect(() => {
+        // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=b7c5d92115fce280b185d643ac4d4dfb&language=en-US
+
+        const fetchData = async() => {
+            const data = await fetch(`${API_URL}/movie/${props.movies.id}/videos?api_key=${API_KEY}&language=en-US`) ;
+            const response = await data.json() ;
+           
+            let videoObject = await response.results.filter((videoObj) => {
+                if (videoObj.type == "Trailer" && videoObj.site == "YouTube") {
+                    // console.log(videoObj); 
+                    return true;
+                }
+                else if(response.results.length == 1 && videoObj.site == "YouTube")
+                {
+                    return true ;
+                }
+                return false;
+            });
+            setObject(videoObject[0]) ;
+        }
+        fetchData() ;
+    })
+
+    // const trailer = `https://www.youtube.com/embed/${vObject.key}?autoplay=1&mute=1&loop=1&modestbranding=1&;controls=0` ;
+    // const trailer = 'https://www.youtube.com/embed/${vObject.key}?autoplay=1' ;
+
 
     return ( 
         <div className="movielist"
-        style={{ left: isHovered && props.index * 225 - 50 + props.index * 2.5 }}
+        // style={{ left: isHovered && props.index * 225 - 50 + props.index * 2.5 }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         >
@@ -26,7 +206,13 @@ const MoviesLists = (props) => {
 
         {isHovered && (
         <>
-            <video className="video" src={trailer} autoPlay={true} loop />
+        <div className="movieHover"
+        style={{ left: isHovered && props.index * 225 - 35 + props.index * 2.5 }}
+        >
+            
+            <iframe className="video-class" src={`https://www.youtube.com/embed/${vObject.key}?autoplay=1&mute=1&loop=1&controls=0&vq=low&modestbranding=1`}></iframe>
+            {/* <YouTube className="video-class" videoId={vObject.key}></YouTube> */}
+            {/* <iframe className="video-class" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1&loop=1&modestbranding=1&;controls=0"></iframe> */}
             <div className="itemInfo">
                 <div className="icons">
                 <PlayArrow className="playy" />
@@ -44,6 +230,7 @@ const MoviesLists = (props) => {
                 </div>
                 <div className="genre">Action</div>
             </div>
+        </div>
         </>
         )}
         </div>
