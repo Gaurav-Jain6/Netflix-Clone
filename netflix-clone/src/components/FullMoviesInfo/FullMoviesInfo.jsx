47eb107e6ddcc5c419dc6d87eb58c8b7e9466b3d
Gaurav <grvjain56@gmail.com>
2022-11-24 15:10:53 +0530

    Movie Content Info Added

--- netflix-clone/src/components/FullMoviesInfo/FullMoviesInfo.jsx
+++ netflix-clone/src/components/FullMoviesInfo/FullMoviesInfo.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import "./FullMoviesInfo.css" ;
 import { useLocation } from "react-router-dom";
 import { API_URL, API_KEY, API_IMAGE } from "../../API/secrets";
+// import {HashLink} from "react-router-hash-link"
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const FullMoviesInfo = () => {
 
     const location = useLocation();
     const { from } = location.state;
-    console.log(from);
+    // console.log(from);
 
     const[vObject , setObject] = useState({}) ;
     const[similarMovieData , setSimilarMovieData] = useState({}) ;
@@ -16,9 +18,13 @@ const FullMoviesInfo = () => {
     // const[director , setDirector] = useState([]) ;
     // const[cast , setCast] = useState([]) ;
     // const[writer , setWriter] = useState([]) ;
+    // const[contentRating , setContentRatingMovieData] = useState([]) ;    
     const[director , setDirector] = useState("") ;
     const[cast , setCast] = useState("") ;
+    const[smallcast , setSmallCast] = useState("") ;
     const[writer , setWriter] = useState("") ;
+    const[contentRating , setContentRatingMovieData] = useState("") ;
+    const[ratingContext , setRatingContext] = useState("") ;    
 
 
     useEffect(() => {
@@ -51,7 +57,7 @@ const FullMoviesInfo = () => {
             const data = await fetch(`${API_URL}/movie/${from.id}/similar?api_key=b7c5d92115fce280b185d643ac4d4dfb&language=en-US&page=1`) ;
             let response = await data.json() ;
             setSimilarMovieData(response) ;
-            console.log(response) ;
+            // console.log(response) ;
         }
         
         const fetchData2 = async() => {
@@ -71,15 +77,13 @@ const FullMoviesInfo = () => {
             setgenre(str) ;
         }
 
-        // https://api.themoviedb.org/3/movie/299534/credits?api_key=b7c5d92115fce280b185d643ac4d4dfb&language=en-US
-
         // https://api.themoviedb.org/3/movie/882598/credits?api_key=b7c5d92115fce280b185d643ac4d4dfb&language=en-US
         const fetchData3 = async() => {
            
             const data = await fetch(`${API_URL}/movie/${from.id}/credits?api_key=b7c5d92115fce280b185d643ac4d4dfb&language=en-US&page=1`) ;
             let response = await data.json() ;
             setCastMovieData(response) ;
-            console.log(response) ;
+            // console.log(response) ;
             
             let DirectorObject = await response.crew.filter((castObj) => {
                 if (castObj.job == "Director") {
@@ -98,12 +102,12 @@ const FullMoviesInfo = () => {
                     director += DirectorObject[i].name + ", " ;
                 }
             }
-            console.log(director) ;
+            // console.log(director) ;
             setDirector(director) ;
             // setDirector(DirectorObject) ;
 
             let writerObject = await response.crew.filter((castObj) => {
-                if (castObj.job == "Writer") {
+                if (castObj.job == "Writer" || castObj.known_for_department == "Writing") {
                     return true;
                 }
                 
@@ -119,7 +123,7 @@ const FullMoviesInfo = () => {
                     writer += writerObject[i].name + ", " ;
                 }
             }
-            console.log(writer) ;
+            // console.log(writer) ;
             setWriter(writer) ;
             // setWriter(writerObject) ;
 
@@ -143,21 +147,99 @@ const FullMoviesInfo = () => {
                     cast += castObject[i].name + ", " ;
                 }
             }
-            console.log(cast) ;
+            // console.log(cast) ;
             setCast(cast) ;
-            // setCast(castObject) ;
+
+            let smallCast = "" ;
+            for(let i = 0 ; i < castObject.length && i < 3; i++)
+            {
+                if(i == castObject.length - 1){
+                    smallCast += castObject[i].name ;
+                }
+                else{
+                    smallCast += castObject[i].name + ", " ;
+                }
+            }
+            // console.log(cast) ;
+            setSmallCast(smallCast) ;
             // console.log(castObject) ;
             // console.log(castObject[0]) ;
             // console.log(castObject) ;            
+        }
+
+        // https://api.themoviedb.org/3/movie/550?api_key=b7c5d92115fce280b185d643ac4d4dfb&append_to_response=releases
+        const fetchData4 = async() => {
+           
+            let rating = "" ;
+            let ratingContext = "" ;
+            const data = await fetch(`${API_URL}/movie/${from.id}?api_key=b7c5d92115fce280b185d643ac4d4dfb&append_to_response=releases`) ;
+            let response = await data.json() ;
+            // console.log(response.releases.countries) ;
+
+            let contentRatingObject = await response.releases.countries.filter((contentRatingObj) => {
+                if (contentRatingObj.iso_3166_1 == "US" && contentRatingObj.certification != "") {
+                    return true;
+                }
+                return false;
+            });
+            rating = contentRatingObject[0].certification ;
+            if(rating.length == 0)
+            {
+                rating = "U/A 13+"
+                ratingContext = "Suitable for persons aged 13 and above and under parental guidance for people under age of 13" ;
+            }  
+            else if(rating == "R")
+            {
+                rating = "U/A 16+"
+                ratingContext = "Suitable for persons aged 16 and above and under parental guidance"
+            }
+            else if(rating == "NC-17")
+            {
+                rating = "A"
+                ratingContext = "Content restricted to adults" 
+            }
+            else if(rating == "G")
+            {
+                rating = "U"
+                ratingContext = "Suitable for children and persons of all ages"
+            }
+            else if(rating == "PG-13")
+            {
+                rating = "U/A 13+"
+                ratingContext = "Suitable for persons aged 13 and above and under parental guidance" ;
+            }
+
+            // console.log(rating) ;
+            setContentRatingMovieData(rating) ;
+            setRatingContext(ratingContext) ;
         }
 
         fetchData() ;
         fetchData1() ;
         fetchData2() ;
         fetchData3() ;
+        fetchData4() ;
     },[])
 
     let str = "" ;
+
+    let {release_date , runtime , vote_average} = from ;
+
+    var hours = Math.floor(runtime / 60);          
+    var minutes = runtime % 60;
+    let val = "" ;
+    if(hours > 0)
+    {
+        val = hours + "hr" + " " + minutes + "min" ;
+    }
+    else if(minutes == 0 )
+    {
+        val = hours + "hr" ;
+    }
+    else
+    {
+        val = minutes + "min" ;
+    }
 
 
     return ( 
@@ -169,7 +251,11 @@ const FullMoviesInfo = () => {
 
             <div className="movie-cast-content-info">
                 <div className="movie-content-info">
-                    <div className="movie-info">
+                    <div className="movie-info-fullContent">
+                        <span className='info_span'>{release_date}</span>
+                        <span className='info_span contentRating'>{contentRating}</span>
+                        <span className='info_span'>{vote_average.toFixed(1)}</span>
+                        <span className='info_span'>{val}</span>
 
                     </div>
                     <div className="movie-description">
@@ -179,7 +265,10 @@ const FullMoviesInfo = () => {
                 </div>
                 <div className="cast-content-info">
                     <div className="upper-cast-info">
-            
+                        <span className='genre-span cast-span'>Cast:</span>
+                        <span className="cast-span-info"> {smallcast}
+                            <AnchorLink href='#about-movie-id'className='anchorLink'> more...</AnchorLink>
+                        </span>
                     </div>
                     <div className="genre-info">
                         <span className='genre-span'>Genres:</span>
@@ -194,7 +283,7 @@ const FullMoviesInfo = () => {
             <div className="about-movie-info">
                 <span className="about-span">About {from.title}</span>
 
-                <div className="about-movie">   
+                <div className="about-movie" id="about-movie-id">   
                     <span className="director about">
                         <span className="general director">Director:</span>
                         <span className="director-info">{director}</span>
@@ -213,6 +302,8 @@ const FullMoviesInfo = () => {
                     </span>
                     <span className="maturity-rating about">
                         <span className="general maturity-rating">Maturity rating:</span>
+                        <span className="general-info contentRating">{contentRating}</span>
+                        <span className="general-info">{ratingContext}</span>
                     </span>
                 </div>
             </div>
